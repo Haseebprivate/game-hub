feabea1a2180aeaf1dd55d0312d614139d9ae514
Haseeb Khalid <haseeb.private@gmail.com>
2023-06-02 01:32:44 +0500

    Fixed the error

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,13 +1,13 @@
 import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
 import React from "react";
-import { Games } from "../hooks/useGames";
+import { Game } from "../hooks/useGames";
 import getCroppedImageUrl from "../services/image-url";
 import CriticScore from "./CriticScore";
 import Emoji from "./Emoji";
 import PlatformIconList from "./PlatformIconList";
 
 interface Props {
-  game: Games;
+  game: Game;
 }
 
 const GameCard = ({ game }: Props) => {
@@ -17,7 +17,9 @@ const GameCard = ({ game }: Props) => {
       <CardBody>
         <HStack justifyContent="space-between" marginBottom={3}>
           <PlatformIconList
-            platforms={game.parent_platforms.map((p) => p.platform)}
+            platforms={game.parent_platforms.map(
+              (platform) => platform.platform
+            )}
           />
           <CriticScore score={game.metacritic} />
         </HStack>
